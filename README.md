3c4f6932aaac5d8f51d6e29c8771491c9c1a21a8
Mark Ulrich <mark.ulrich.86@gmail.com>
2020-11-26 16:40:36 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Github Issue Manager
+
+A tool to create, read, update and delete GitHub issues from the command line.
+
+Built for Exercise 4.11 from The Go Programming Language.
---
+++ main.go
@@ -0,0 +1,7 @@
+// A tool to create, read, update, and delete GitHub issues from the command
+// line. Built for exercise 4.11 from The Go Programming Language.
+package main
+
+func main() {
+
+}
